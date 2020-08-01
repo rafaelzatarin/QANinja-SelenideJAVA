0025f8f9e4c299bd21b756f568eb5e4184f51804
rafaelzatarin <rafadecps@gamil.com>
2020-08-01 09:59:56 -0300

    Realizado ajustes no projeto e implementação da classe HelloSelenideTest

--- ninjaplus/src/test/java/HelloSelenideTest.java
+++ ninjaplus/src/test/java/HelloSelenideTest.java
@@ -1,11 +1,16 @@
 import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.title;
 import static com.codeborne.selenide.WebDriverRunner.isChrome;
+import static org.testng.Assert.assertEquals;
 
 public class HelloSelenideTest {
 
     @Test
     public void OnAir() {
         isChrome();
-        open('h')
+        open("http://ninjaplus-web:5000/login");
+        assertEquals(title(), "Ninja+");
     }
 }
